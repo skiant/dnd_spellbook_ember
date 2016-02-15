0610765bf47ca6bcf4f9c53ea5ee0ab8423a4566
Mathieu Dubois <skiant.be@gmail.com>
2016-02-15 21:38:58 +0100

    Fix passing levels to the edit spell template

--- app/routes/spells/edit.js
+++ app/routes/spells/edit.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    // return this.store.find('spell', this.spell_id);
-    let spell_id = this.spell_id;
-    return Ember.RSVP.hash({
-      spell: this.store.find('spell', spell_id),
-      levels: this.store.findAll('level'),
-    });
+  model(params) {
+    return this.store.find('spell', params.spell_id);
+  },
+
+  setupController(controller, model) {
+    this._super(controller,model);
+    controller.set('levels', this.store.findAll('level'));
   }
 });
